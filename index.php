18de480decf5668518c5c7e6873424cc19ebf3c6
Jose Quizon <josequizon@hotmail.com>
2017-10-19 11:53:04 -0400

    php index for php loading

---
+++ index.php
@@ -0,0 +1,214 @@
+
+
+<!doctype html>
+<html>
+	<head>
+		<title>
+			Jose Quizon Portfolio
+		</title>
+		<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="js/pfolio.js"></script>
+<!-- 		<link rel="stylesheet" type="text/css" href="css/jcqfolio.css"> -->
+		<link rel="stylesheet" type="text/css" href="css/jcqfolioNEW.css">
+		<link rel="stylesheet" type="text/css" href="fonts/font.css">		
+		 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+	</head>
+	<body id="home">
+<!-- ___________TITLE_________________________________ -->
+
+		<div class="titleholder">
+
+			<p id="title">
+				JOSE QUIZON.<span id ="occup">DESIGNER & CODER.</span>
+			</p>
+
+		</div>
+<!-- ______________ACTUAL WORK________________________ -->
+	<!-- back to grid switch -->
+	<div class="gridreturn" id="backtogrid">
+		<svg  id="gridretsvg" x="0px" y="0px" width="70px" height="70px" viewBox="0 0 70 70" xml:space="preserve">
+			<g>
+				<rect x="1.311" y="1.311" fill="#FFFFFF" stroke="#000000" stroke-width="2" width="26.379" height="26.379"/>
+				<rect x="41.311" y="1.311" fill="#FFFFFF" stroke="#000000" stroke-width="2" width="26.379" height="26.379"/>
+				<rect x="1.311" y="41.311" fill="#FFFFFF" stroke="#000000" stroke-width="2" width="26.379" height="26.379"/>
+				<rect x="41.311" y="41.311" fill="#FFFFFF" stroke="#000000" stroke-width="2" width="26.379" height="26.379"/>
+			</g>
+		</svg>
+	</div>
+
+
+
+
+
+<div class="workactual" id="momathDIV">
+
+</div>
+		
+
+
+	<div class="workactual" id="seasaltDIV">
+
+
+
+	</div>
+
+	
+
+<!--________________ICONS____________________________  -->
+		<div id="logohold">
+				<svg class="iconlinks" id="skills" x="0px" y="0px" width="97.905px" height="74.58px" viewBox="0 0 97.905 74.58" xml:space="preserve">
+					<g>
+						<path fill="none" stroke="#000000" stroke-width="4" d="M68.61,30.509c0,0,6.713,10.742,19.633,2.669c12.92-8.074,8.395-20.183,8.395-20.183
+							l-15.106,9.44l-4.946-7.915l15.106-9.44c0,0-8.898-9.375-21.818-1.302c-12.92,8.074-6.208,18.815-6.208,18.815l-34.37,21.478
+							c0,0-6.713-10.742-19.633-2.669c-12.92,8.074-8.394,20.182-8.394,20.182l15.106-9.439l4.946,7.915L6.213,69.498
+							c0,0,8.899,9.377,21.819,1.302c12.92-8.072,6.208-18.813,6.208-18.813L68.61,30.509z"/>
+					</g>
+				</svg>			
+	
+							
+				<a  href="JoseQuizon.pdf">			
+					<svg  class="iconlinks" id="resume"x="0px" y="0px" width="99.64px" height="76.453px" viewBox="0 0 99.64 76.453" xml:space="preserve">
+
+								<polygon fill="none" stroke="#000000" stroke-width="4" points="96.167,25.62 96.167,75.953 0.5,75.953 0.5,3.453 71,3.453 71.167,25.786 "/>
+								<polyline fill="none" stroke="#000000" stroke-width="4" points="96.167,25.62 98.333,25.62 71,1.12 71,3.453 "/>
+													
+					</svg>
+				</a>
+				<a href="http://jcqfolio.com/contactform.php">
+					<svg  class="iconlinks" id="contact"x="0px" y="0px" width="101.894px" height="75.833px" viewBox="0 0 101.894 75.833" xml:space="preserve">
+							<polygon fill="none" stroke="#000000" stroke-width="4" points="1.197,0.5 50.942,50.833 100.697,0.5 "/>
+							<rect x="1.197" y="0.5" fill="none" stroke="#000000" stroke-width="4" width="99.963" height="74.833"/>
+							<line fill="none" stroke="#000000" stroke-width="4" x1="30.736" y1="30.422" x2="1.197" y2="75.333"/>
+							<line fill="none" stroke="#000000" stroke-width="4" x1="71.623" y1="30.422" x2="101.161" y2="75.333"/>
+					</svg>
+				</a>
+		</div>
+
+
+<!-- _____SKILLS______________________________________ -->
+		<div class="skillsmodalA">
+			<p id="skillsttl">
+				Skills
+			</p>
+			<p>
+				<br>
+				Illustrator
+				</br>
+				<br>
+				Photoshop 
+				</br>
+				<br>
+				In Design
+				</br>
+				<br>
+				Flash
+				</br>
+				<br>
+				Lightwave
+				</br>
+				<br>
+				PHP
+				</br>
+				<br>
+				HTML
+				</br>
+				<br>
+				CSS
+				</br>
+				<br>
+				JQuery
+				</br>
+				<br>
+				Infographics 
+				</br>
+				<br>
+				Package design 
+				</br>
+				<br>
+				Branding 
+				</br>
+				<br>
+				Front End dev.
+			</br></p>
+
+		</div>			
+
+
+
+<!-- ____________OPENING PAGE ICONS __________________ -->
+
+
+		<div class="workhold">
+			<div class="workgrid" id="work1A">
+				<!-- <img src="images/galleryfolder/work3A.jpg"> -->
+			<p class="workgridtitles">
+				MARITIME HOTEL<br>
+				Commisioned Work			
+				</P>
+			</div>
+
+			<div class="workgrid" id="work1B">
+				<P class="workgridtitles">
+					GAME OF THRONES<br>
+					Study
+				</P>
+			</div>
+			<div class="workgrid" id="work1C">
+				<P class="workgridtitles">
+					MARVEL'S X-MEN<br>
+					Study
+
+				</P>
+			</div>
+			<div name ="momath" class="workgrid" id="momath" value="momath">
+				<P class="workgridtitles">
+					MOMATH<br>
+					Student Work
+							<img class="gridimg" src="images/gallerymomath/momathworkA.jpg">
+				</P>
+			</div>
+			<div class="workgrid" id="seasalt">
+				<P class="workgridtitles">
+					SEASALT BBQ RUB<br>
+					Student Work
+							<img class="gridimg" src="images/galleryseasalt/seasaltworkA.jpg">
+				</P>
+			</div>
+			<div class="workgrid" id="work1F">
+				<P class="workgridtitles">
+					JCAS - 3<br>
+					Student Work
+				</P>
+			</div>
+
+			<div class="workgrid" id="work2A">
+				<P class="workgridtitles">
+					GHOSTDUST MUSIC<br>
+					Student WorK
+				</P>
+			</div>
+			<div class="workgrid" id="work2B">
+				<P class="workgridtitles">
+					FOX'S 24<br>
+					Student Work
+				</P>
+			</div>
+			<div class="workgrid" id="work2C">
+				<P class="workgridtitles">
+					OLYMPICS<br>
+					Student Work
+				</P>
+			</div>
+			<div class="workgrid" id="work2D">
+				<P class="workgridtitles">
+					NIKE<br>
+					Student Work
+				</P>
+			</div>
+		</div>
+
+
+	</body>
+</html>
